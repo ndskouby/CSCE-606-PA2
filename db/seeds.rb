5c3a9356555c4e2af9852469f389c8404669abc3
Nathan Skouby <ndskouby@gmail.com>
2024-02-20 23:59:05 -0600

    updated seed data and release seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Interstellar', :rating => 'PG-13',
+    :release_date => '5-Nov-2014'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '16-Jul-2010'},
+  {:title => 'Logan', :rating => 'R',
+    :release_date => '3-Mar-2017'}
 ]
 
 more_movies.each do |movie|
--- release_tasks.sh
+++ release_tasks.sh
@@ -4,6 +4,6 @@ echo "Runnning database migrations ..."
 bundle exec rake db:migrate
 
 echo "Seeding the database..."
-bundle exec rake db:seed
+bundle exec rake db:seed:replant
 
 echo "Release tasks completed."
